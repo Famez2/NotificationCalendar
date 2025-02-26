ed395096fb3680417f98805fec48a0365e066d4c
Famez1 <a.plyakin@sh.dunice.net>
2025-02-26 09:07:47 +0300

    Добавлена валидация на регистрацию пользователя

---
+++ src/NotificationCalendar.Application/Handlers/Authentication/Commands/SignUpUser/SingUpUserCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+
+namespace NotificationCalendar.Application.Handlers.Authentication.Commands.SignUpUser;
+
+public class SingUpUserCommandValidator : AbstractValidator<SingUpUserCommand>
+{
+    public SingUpUserCommandValidator()
+    {
+        RuleFor(c => c.Email)
+            .NotEmpty()
+            .WithMessage("Email не должен быть пустым");
+
+        RuleFor(c => c.Password)
+            .NotEmpty()
+            .WithMessage("Пароль не должен быть пустым");
+    }
+}
